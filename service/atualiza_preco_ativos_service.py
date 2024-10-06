05a40c6e5d1cec29899ae3583bd86f187fe6b8d4
André de Melo <andrebrmelo@gmail.com>
2024-10-06 12:09:04 -0300

    atualizando valores dos fundos de renda fixa e planilha de dividendos para serem importados.

--- service/atualiza_preco_ativos_service.py
+++ service/atualiza_preco_ativos_service.py
@@ -15,9 +15,9 @@ def atualiza_preco_atual():
         if classe != 'renda_fixa':
             preco_atual = round(yahoo_finance.obtem_preco_atual(ticker, classe), 2)
         elif classe == 'renda_fixa' and ticker == 'SUL AMERICA EXCLUSIVE FI RF REF DI':
-            preco_atual = 111.92
+            preco_atual = 112.40
         elif classe == 'renda_fixa' and ticker == 'TREND DI FIC FI RF SIMPLES':
-            preco_atual = 1.26
+            preco_atual = 1.27
         elif classe == 'renda_fixa' and ticker == 'TREND INB FIC FI RF SIMPLES':
             preco_atual = 1.44
         if classe == 'etf_eua': 
