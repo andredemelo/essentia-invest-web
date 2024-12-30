1674a4ca5fd5474bc35036a9962157872c6cfb88
André de Melo <andrebrmelo@gmail.com>
2024-12-30 14:13:03 -0300

    Incluindo gráfico de movimentação mensal.

--- service/transacao_service.py
+++ service/transacao_service.py
@@ -1,8 +1,11 @@
 from flask import render_template, redirect, url_for, flash, session, request
 from werkzeug.utils import secure_filename
+from util import conversor
+from collections import defaultdict
+from datetime import datetime
+
 import os
 import pandas as pd
-from util import conversor
 import app
 import repository.transacao_repository as transacao_repository
 
@@ -48,6 +51,7 @@ def lista_transacoes():
     consulta_transacoes = transacao_repository.listar_todas_transacoes()
     transacoes = {}
     totais = {'compras': 0, 'vendas': 0, 'bonificacoes': 0}
+    totais_por_mes = defaultdict(lambda: {'compras': 0, 'vendas': 0, 'bonificacoes': 0})
 
     for transacao in consulta_transacoes:
         codigo_ativo = transacao[4]
@@ -55,6 +59,10 @@ def lista_transacoes():
         quantidade = conversor.convert_to_float(transacao[6])
         preco_unitario = conversor.convert_to_float(transacao[7])
         valor = round(preco_unitario * quantidade, 2)
+        
+        # Data da operação formatada
+        data_operacao = datetime.strptime(transacao[2], "%d/%m/%Y")
+        mes_ano = data_operacao.strftime("%Y-%m")
 
         # Organização de transações por ativo
         if codigo_ativo not in transacoes:
@@ -65,12 +73,24 @@ def lista_transacoes():
         # Cálculo dos totais por tipo de operação
         if operacao == 'C':
             totais['compras'] += valor
+            totais_por_mes[mes_ano]['compras'] += valor
         elif operacao == 'V':
             totais['vendas'] += valor
+            totais_por_mes[mes_ano]['vendas'] += valor
         elif operacao == 'B':
             totais['bonificacoes'] += valor
+            totais_por_mes[mes_ano]['bonificacoes'] += valor
 
     totais = {k: round(v, 2) for k, v in totais.items()}
     total_movimentacao = round(totais['compras'] - totais['vendas'], 2)
 
-    return render_template('transacoes.html', transacoes=transacoes, totais=totais, total_movimentacao=total_movimentacao)
+    # Converte totais por mês para JSON serializável
+    totais_por_mes = dict(sorted(totais_por_mes.items()))
+
+    return render_template(
+        'transacoes.html',
+        transacoes=transacoes,
+        totais=totais,
+        total_movimentacao=total_movimentacao,
+        totais_por_mes=totais_por_mes
+    )
