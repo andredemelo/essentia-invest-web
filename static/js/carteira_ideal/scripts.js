dca84a7f3a8ae52642d2a9c7150791234f0fa50c
André de Melo <andrebrmelo@gmail.com>
2025-01-05 13:22:48 -0300

    Corrigindo modais da funcionalidade de carteira ideal.

--- static/js/carteira_ideal/scripts.js
+++ static/js/carteira_ideal/scripts.js
@@ -1,5 +1,33 @@
-const ctx = document.getElementById('graficoPizza').getContext('2d');
+// Modais
+const labels = Object.keys(alocacoesJson);
+const data = Object.values(alocacoesJson);
 
+        // Evento para abrir modal de edição com valores preenchidos
+        $('#editarModal').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget);
+            var classeAtivo = button.data('classe');
+            var porcentagem = button.data('porcentagem');
+            var dividendo_desejado = button.data('dividendo');
+
+            var modal = $(this);
+            modal.find('.modal-body #classe_ativo_modal_editar').val(classeAtivo);
+            modal.find('.modal-body #porcentagem_modal_editar').val(porcentagem);
+            modal.find('.modal-body #dividendo_desejado_editar').val(dividendo_desejado);
+            modal.find('.modal-body #classe_ativo_hidden_editar').val(classeAtivo);
+        });
+
+        // Evento para abrir modal de exclusão com o valor correto
+        $('#excluirModal').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget);
+            var classeAtivo = button.data('classe');
+
+            var modal = $(this);
+            modal.find('#classe_ativo_modal_excluir').text(classeAtivo);
+            modal.find('#classe_ativo_hidden_excluir').val(classeAtivo);
+        });
+
+// Gráfico de Pizza
+const ctx = document.getElementById('graficoPizza').getContext('2d');
 if (typeof alocacoesJson !== 'undefined') {
     new Chart(ctx, {
         type: 'pie',
